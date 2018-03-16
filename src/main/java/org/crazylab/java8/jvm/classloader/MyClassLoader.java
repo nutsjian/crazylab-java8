3e8cbd77ca2dd84f41f8e4b110cc6be41c7a6692
周丽雯 <Emily@zhouliwendeMacBook-Air.local>
2018-03-16 08:57:19 +0800

    update

---
+++ src/main/java/org/crazylab/java8/jvm/classloader/MyClassLoader.java
@@ -0,0 +1,16 @@
+package org.crazylab.java8.jvm.classloader;
+
+public class MyClassLoader extends ClassLoader {
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        // 获取类的字节数组
+        byte[] classBytes = getClassBytes(name);
+        if (classBytes == null) {
+            throw new ClassNotFoundException(name);
+        } else {
+            // 调用 defineClass 方法生成相应的 Class 对象
+            return defineClass(name, classBytes, 0, classBytes.length);
+        }
+    }
+
+}
